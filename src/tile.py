c7b35d792d90ec00cce5920c9ecda2456c603b8b
Th1nhNg0 <ngophuthinh2001@gmail.com>
2024-08-19 00:21:48 +0700

    Refactor connection logic for tiles

--- src/tile.py
+++ src/tile.py
@@ -13,53 +13,42 @@ class Position(NamedTuple):
         return Position(self.x - other.x, self.y - other.y)
 
 
-class DIRECTION(IntEnum):
+class Direction(IntEnum):
     TOP, RIGHT, BOTTOM, LEFT = range(4)
 
     @property
     def delta(self):
         return {
-            DIRECTION.TOP: Position(0, -1),
-            DIRECTION.RIGHT: Position(1, 0),
-            DIRECTION.BOTTOM: Position(0, 1),
-            DIRECTION.LEFT: Position(-1, 0),
+            Direction.TOP: Position(0, -1),
+            Direction.RIGHT: Position(1, 0),
+            Direction.BOTTOM: Position(0, 1),
+            Direction.LEFT: Position(-1, 0),
         }[self]
 
     @property
     def opposite(self):
-        return DIRECTION((self + 2) % 4)
+        return Direction((self + 2) % 4)
 
 
-class TileName(IntEnum):
+class Tile(IntEnum):
     EMPTY = 0
+
     CURVE_BR = 1
-    CURVE_RB = 1  # same as CURVE_BR
     CURVE_BL = 2
-    CURVE_LB = 2  # same as CURVE_BL
     CURVE_TL = 3
-    CURVE_LT = 3  # same as CURVE_TL
     CURVE_TR = 4
-    CURVE_RT = 4  # same as CURVE_TR
 
     STRAIGHT_V = 5
     STRAIGHT_H = 6
 
     T_TURN_VBL = 7
-    T_TURN_VLB = 7  # same as T_TURN_V_BL
     T_TURN_HLT = 8
-    T_TURN_HTL = 8  # same as T_TURN_H_LT
     T_TURN_VTR = 9
-    T_TURN_VRT = 9  # same as T_TURN
     T_TURN_HBR = 10
-    T_TURN_HRB = 10  # same as T_TURN_H_BR
     T_TURN_VTL = 11
-    T_TURN_VLT = 11  # same as T_TURN_V_TL
     T_TURN_HRT = 12
-    T_TURN_HTR = 12  # same as T_TURN_H_RT
     T_TURN_VRB = 13
-    T_TURN_VBR = 13  # same as T_TURN_V_RB
     T_TURN_HLB = 14
-    T_TURN_HBL = 14  # same as T_TURN_H_LB
 
     FENCE = 15
 
@@ -103,3 +92,48 @@ class TileName(IntEnum):
     @classmethod
     def is_t_turn(cls, tile):
         return tile in cls.t_turn_tiles()
+
+    def get_connection_direction(self):
+        result = {
+            Direction.TOP: False,
+            Direction.RIGHT: False,
+            Direction.BOTTOM: False,
+            Direction.LEFT: False,
+        }
+        d = self.name.split("_")
+        if len(d) == 1:
+            return result
+        d = d[-1]
+        if "V" in d:
+            result[Direction.TOP] = True
+            result[Direction.BOTTOM] = True
+        if "H" in d:
+            result[Direction.RIGHT] = True
+            result[Direction.LEFT] = True
+        if "T" in d:
+            result[Direction.TOP] = True
+        if "B" in d:
+            result[Direction.BOTTOM] = True
+        if "L" in d:
+            result[Direction.LEFT] = True
+        if "R" in d:
+            result[Direction.RIGHT] = True
+        return result
+
+
+def make_connectable_dict():
+    data = {}
+    for tile_src in Tile:
+        src_connection_direction = tile_src.get_connection_direction()
+        data[tile_src] = {}
+        for tile_dst in Tile:
+            dst_connection_direction = tile_dst.get_connection_direction()
+            data[tile_src][tile_dst] = {}
+            for direction in Direction:
+                data[tile_src][tile_dst][direction] = False
+                if (
+                    src_connection_direction[direction]
+                    and dst_connection_direction[direction.opposite]
+                ):
+                    data[tile_src][tile_dst][direction] = True
+    return data
